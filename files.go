30c25279267e414352d6b07487b4fc0c4252c20f
Alexander Ponomarev <ponomarev7.aa@edu.spbstu.ru>
2023-11-06 02:28:43 +0300

    Add go doc for Contains function

--- files.go
+++ files.go
@@ -5,6 +5,7 @@ import (
 	"os"
 )
 
+// Contains checks if file data contains a substring
 func Contains(filepath string, substring string) (bool, error) {
 	file, err := os.ReadFile(filepath)
 	if err != nil {
